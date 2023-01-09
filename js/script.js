30ef2a6c169fae9b3619ecec734012a757f355e2
Alberto-Bruno <brunoalberto97@outlook.it>
2023-01-09 18:33:55 +0100

    message , image

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-console.log('Vue OK', Vue)
+console.log('Vue OK', Vue);
 
 const app = Vue.createApp({
   data() {
-    
+    return {
+        message: 'Hello Vuejs!',
+            image: './img/logo-vue.jpg'
+    }
   },
 });
 
